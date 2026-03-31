41b1b64e10b3e2150c24251b14bc6e87c91c9e9f
aditohates-bugs <adityabanerjeevk@gmail.com>
2026-03-31 13:25:53 +0530

    very unmade

---
+++ game.js
@@ -0,0 +1,13 @@
+const start = document.getElementById("START");
+
+function starSys(){
+
+}
+
+start.addEventListener("click", ()=>{
+    setTimeout(window.location.href = "rounds.html", 5000);
+});
+
+function zoom(){
+    
+}
